30737423585d7aaa97b0be6f3be64819c89a921f
tk08540 <tk08540@student.uni-lj.si>
2025-11-20 21:46:35 +0100

    Starting categories in the database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using FinFriend.Models;
+using System.ComponentModel;
 namespace FinFriend.Data;
-
+using FinFriend.Models;
+using static FinFriend.Models.TransactionType;
 public class ApplicationDbContext : IdentityDbContext<User>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
@@ -24,7 +26,7 @@ public class ApplicationDbContext : IdentityDbContext<User>
         modelBuilder.Entity<Category>().ToTable("Categories");
         modelBuilder.Entity<Transaction>().ToTable("Transactions");
         
-        // Configure relationships for transactions where an account can be source or destination
+        // odnosi med racuni in transakcijami, da se baza ne zmede
         modelBuilder.Entity<Account>()
             .HasMany(a => a.SourceTransactions)
             .WithOne(t => t.SourceAccount)
@@ -36,5 +38,30 @@ public class ApplicationDbContext : IdentityDbContext<User>
             .WithOne(t => t.DestinationAccount)
             .HasForeignKey(t => t.DestinationAccountId)
             .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Category>().HasData(
+            //defaultne kategorije stroskov
+            new Category { CategoryId = 1, Name = "Rent", Type = Expense, UserId = null },
+            new Category { CategoryId = 2, Name = "Bills", Type = Expense, UserId = null },
+            new Category { CategoryId = 3, Name = "Taxes", Type = Expense, UserId = null },
+            new Category { CategoryId = 4, Name = "Essentials", Type = Expense, UserId = null },
+            new Category { CategoryId = 5, Name = "Health", Type = Expense, UserId = null },
+            new Category { CategoryId = 6, Name = "Transport", Type = Expense, UserId = null },
+            new Category { CategoryId = 7, Name = "Eating Out", Type = Expense, UserId = null },
+            new Category { CategoryId = 8, Name = "Entertainment", Type = Expense, UserId = null },
+            new Category { CategoryId = 9, Name = "Other Expense", Type = Expense, UserId = null },
+
+            //defaultne kategorije prihodkov
+            new Category { CategoryId = 10, Name = "Salary", Type = Income, UserId = null },
+            new Category { CategoryId = 11, Name = "Other Income", Type = Income, UserId = null },
+
+
+
+            //Defaultne kategorije transferjev
+            new Category { CategoryId = 12, Name = "Withdrawal", Type = Transfer, UserId = null },
+            new Category { CategoryId = 13, Name = "Deposit", Type = Transfer, UserId = null },
+            new Category { CategoryId = 14, Name = "Bank Transfer", Type = Transfer, UserId = null }
+        );
+       
     }
 }
--- Migrations/20251120204428_StartingCategories.Designer.cs
+++ Migrations/20251120204428_StartingCategories.Designer.cs
@@ -4,6 +4,7 @@ using FinFriend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinFriend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251120204428_StartingCategories")]
+    partial class StartingCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
