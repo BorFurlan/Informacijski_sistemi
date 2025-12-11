3fc3d566646ae550fdf0767e8c8698ec6ef26b44
tk08540 <tk08540@student.uni-lj.si>
2025-12-11 15:08:09 +0100

    home page skupno in racuni

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,16 +23,28 @@ public class HomeController : Controller
     
     public async Task<IActionResult> Index()
     {
-        // var userId = _userManager.GetUserId(User);
+        var userId = _userManager.GetUserId(User);
 
-        // // Prikaže samo račune prijavljenega uporabnika, ki so označeni kot "included"
-        // var totalBalance = await _context.Accounts
-        //     .Where(a => a.UserId == userId && a.IsIncludedInTotal == true)
-        //     .SumAsync(a => a.CurrentBalance);
+        IQueryable<Account> accountsQuery = _context.Accounts.Where(a => a.UserId == userId);
 
-        // ViewBag.TotalBalance = totalBalance;
+        var accounts = await accountsQuery.ToListAsync();
+        var vm = new DashboardViewModel
+        {
+            TotalBalance = accounts.Where(a => a.IsIncludedInTotal).Sum(a => a.CurrentBalance),
+            Accounts = accounts.Select(a => new DashboardViewModel.AccountInfo
+            {
+                AccountId = a.AccountId,
+                Name = a.Name,
+                Type = a.Type,
+                InitialBalance = a.InitialBalance,
+                CurrentBalance = a.CurrentBalance,
+                IsIncludedInTotal = a.IsIncludedInTotal
+            }).ToList()
+        };
+        // Prikaže samo račune prijavljenega uporabnika, ki so označeni kot "included"
+    
 
-        return View();
+        return View(vm);
     }
 
     public IActionResult Privacy()
